38de7eec8b32777971d221affc8f4165c3ffd7fe
Daniel Sanchez <d0xcodex0s@gmail.com>
2019-01-31 09:53:19 +0100

    cargando peliculas de la base de datos

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -6,19 +6,19 @@
 
         <div class="col-sm-4">
 
-            <a href="{{ url('/catalog/show/' . $id ) }}">
+            <a href="{{ url('/catalog/show/' . $pelicula->id ) }}">
                 <img src="{{$pelicula['poster']}}" style="height:200px"/>
             </a>
 
         </div>
         <div class="col-sm-8">
 
-            <h4>{{$pelicula['title']}}</h4>
-            <h6>A&ntilde;o: {{$pelicula['year']}}</h6>
-            <h6>Director: {{$pelicula['director']}}</h6>
-            <p><strong>Resumen:</strong> {{$pelicula['synopsis']}}</p>
+            <h4>{{$pelicula->title}}</h4>
+            <h6>A&ntilde;o: {{$pelicula->year}}</h6>
+            <h6>Director: {{$pelicula->director}}</h6>
+            <p><strong>Resumen:</strong> {{$pelicula->synopsis}}</p>
             <p><strong>Estado: </strong>
-                @if($pelicula['rented'])
+                @if($pelicula->rented)
                     Pel&iacute;cula actualmente alquilada.
                 @else
                     Pel&iacute;cula en stock.
@@ -26,7 +26,7 @@
             </p>
 
             <a class="btn btn-danger" href="#">Devolver pel&iacute;cula</a>
-            <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $id ) }}">
+            <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $pelicula->id ) }}">
                 <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                 Editar pel&iacute;cula</a>
             <a class="btn btn-outline-info" href="{{ action('CatalogController@getIndex') }}">Volver al listado</a>
